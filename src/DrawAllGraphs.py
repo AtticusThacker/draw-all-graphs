3bac01d70dcca97f2e3e7990cc38fd0b8a340ee5
AtticusThacker <138910121+AtticusThacker@users.noreply.github.com>
2024-05-03 12:34:33 -0400

    add BitGraph class to improve performance, next step is improving compute_binary_permutations

--- src/DrawAllGraphs.py
+++ src/DrawAllGraphs.py
@@ -8,11 +8,41 @@ import networkx as nx
 import matplotlib.pyplot as plt 
 import itertools
 
-class Graph:
-  def __init__(self, vertices, edges, string):
-    self.vertices = vertices
-    self.edges = edges
+#from pyavl import AvlTree
+
+class BitGraph:
+  def __init__(self, string):
     self.string = string
+
+  #this checks equality between two strings
+  def __eq__(self, other):
+    #iterates through both lists simultaneously
+    for i,j in zip(self.string, other.string):
+      if i ^ j:
+        return False
+    return True
+
+  #hashes the bitgraph to an integer
+  def __hash__(self):
+    res = 0
+    for ele in self.string:
+      res = (res << 1) | ele
+    return res
+
+  #finds all unique, isomorphic bitgraphs to this one
+  def isomorphic_sequences(self, edge_relabellings):
+    isomorphic_sequences = set()
+    for permutation in edge_relabellings:
+      new_array = []
+      for i in range(len(self.string)):
+        new_array.append(self.string[permutation[i]])
+      isomorphic_sequences.add(BitGraph(new_array))
+    return isomorphic_sequences
+
+  def len(self):
+    return len(self.string)
+  
+
 
 
 # compute_binary_permutations(p,q):
@@ -20,27 +50,27 @@ class Graph:
 def compute_binary_permutations(p, q):
   def backtrack(curr_permutation, zeros_remaining, ones_remaining):
     if zeros_remaining == 0 and ones_remaining == 0:
-      unique_permutations.append(curr_permutation[:])
+      unique_permutations.add(BitGraph(curr_permutation[:]))
       return
     if zeros_remaining > 0:
-      curr_permutation.append(0)
+      curr_permutation.append(False)
       backtrack(curr_permutation, zeros_remaining - 1, ones_remaining)
       curr_permutation.pop()
     if ones_remaining > 0:
-      curr_permutation.append(1)
+      curr_permutation.append(True)
       backtrack(curr_permutation, zeros_remaining, ones_remaining - 1)
       curr_permutation.pop()
 
-  unique_permutations = []
+  unique_permutations = set()
   backtrack([], (p*(p-1))//2-q, q)
   return unique_permutations
     
 
 # compute_vertex_relabellings(p):
 # precompute all the permutations of [0...p-1] and return them nested in a list
-def compute_vertex_relabellings(p):
-  permutations_list = list(itertools.permutations(range(p)))
-  return permutations_list
+#def compute_vertex_relabellings(p):
+#  permutations_list = list(itertools.permutations(range(p)))
+#  return permutations_list
 
   
 # edge_to_index(v1,v2):
@@ -51,12 +81,12 @@ def edge_to_index(v1, v2):
   return (y**2 - y + 2*x) // 2
 
 
-# precompute_edge_relabellings(p,vertex_relabellings):
+# precompute_edge_relabellings(p):
 # for each given permutation of [0...p-1], calculate a list describing which index each edge goes to after permuting the vertices
-def precompute_edge_relabellings(p,vertex_relabellings):
+def precompute_edge_relabellings(p):
   master_list = []
   permuted_edges = [None] * ((p*(p-1))//2)
-  for permutation in vertex_relabellings:
+  for permutation in itertools.permutations(range(p)):
     for i in range(1, p):
       for j in range(i):
         permuted_edges[edge_to_index(permutation[i], permutation[j])] = edge_to_index(i, j)
@@ -86,17 +116,24 @@ def list_isomorphic_sequences(binary_sequence, edge_relabellings):
   return eliminate_duplicate_lists(isomorphic_sequences)
   
 
+def to_integer(binary_sequence):
+    res = 0
+    for ele in binary_sequence:
+        res = (res << 1) | ele
+
+
 # group_isomorphic_sequences(binary_sequences, edge_relabellings):
 # start with two lists "grouped_sequences" and "binary_sequences". 
 # for each binary sequence in binary_sequences, compute list_isomorphic_sequences(binary_sequence, edge_relabellings) and append the list of all isomorphic sequences to grouped_sequences in a nested list while also removing each of them from binary_sequences, repeat this until binary_sequences is empty.
 def group_isomorphic_sequences(binary_sequences, edge_relabellings):
   grouped_sequences = []
+
   while len(binary_sequences) > 0:
-    for binary_sequence in binary_sequences:
-      x=list_isomorphic_sequences(binary_sequence, edge_relabellings)
-      grouped_sequences.append(x.copy())
-      for y in x:
-        binary_sequences.remove(y)
+    binary_sequence = binary_sequences.pop()
+    x = binary_sequence.isomorphic_sequences(edge_relabellings)
+    grouped_sequences.append(x)
+    for y in x:
+      binary_sequences.discard(y)
   return grouped_sequences
 
 
@@ -104,13 +141,13 @@ def group_isomorphic_sequences(binary_sequences, edge_relabellings):
 def print_one_of_each(grouped_sequences):
   seqs=[]
   for string in grouped_sequences:
-    seqs.append(string[0])
+    seqs.append(string.pop())
   return seqs
 
 # do the final calculation all in one function
 def calculate_isomorphic_sequences(p, q):
   b = compute_binary_permutations(p, q)
-  e = precompute_edge_relabellings(p,compute_vertex_relabellings(p))
+  e = precompute_edge_relabellings(p)
   return print_one_of_each(group_isomorphic_sequences(b, e))
 # -----------------------------------
 
@@ -148,16 +185,18 @@ class GraphVisualization:
         plt.show() 
 
 
-def draw_graph(sequence):
+def draw_graph(graph):
   G = GraphVisualization() 
+  sequence = graph.string
   for i in range(len(sequence)):
-    if sequence[i] == 1:
+    if sequence[i]:
       c=index_to_edge(i)
       G.addEdge(c[0], c[1])
   G.visualize() 
 
-graphs=calculate_isomorphic_sequences(6, 9)
+graphs=calculate_isomorphic_sequences(8, 12)
 
 print(graphs)
 
-draw_graph(graphs[3])
+for i in range(min(10,len(graphs))):
+    draw_graph(graphs[i])
